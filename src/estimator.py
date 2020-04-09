7cf06d3f4bfc4bc7d514dbd78310ab8dee1ef505
kamula <kamulaisaac@gmail.com>
2020-04-09 22:27:26 +0300

    challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,32 @@
+import math
+
+
 def estimator(data):
-  return data
+    # function to calculate the number of currently infected people
+    def current_infections():
+        return int(data['reportedCases'] * 10)
+
+    # function to calculate 50 * more people infected
+    def more_people():
+        return int(data['reportedCases'] * 50)
+
+    # infectionsByRequestedTime for impact['currently infected]
+    def infections_by_time(days):
+        infections = current_infections()
+        infectionsByRequestedTime = infections * math.pow(2, math.floor(days / 3))
+
+    # infections for servere impact
+    def infections_by_severe_impact(days):
+        infections = more_people()
+        infectionsByRequestedTime = infections * math.pow(2, math.floor(days / 3))
+
+    return {
+        data: {},
+        "impact": {
+            "currentlyInfected": current_infections()
+
+        },
+        "severeImpact": {
+            "currentlyInfected": more_people()
+        }
+    }
