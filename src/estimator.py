37f53f750dfadcc4a04c32076c2e86b66c934ec0
kamula <kamulaisaac@gmail.com>
2020-04-09 22:43:41 +0300

    truncate

--- src/estimator.py
+++ src/estimator.py
@@ -13,13 +13,13 @@ def estimator(data):
     # infectionsByRequestedTime for impact['currently infected]
     def infections_by_time(days):
         infections = current_infections()
-        infectionsByRequestedTime = infections * math.pow(2, math.floor(days / 3))
+        infectionsByRequestedTime = infections * math.pow(2, math.trunc(days / 3))
 
     # infections for servere impact
     def infections_by_severe_impact(days):
         infections = more_people()
-        infectionsByRequestedTime = infections * math.pow(2, math.floor(days / 3))
-        
+        infectionsByRequestedTime = infections * math.pow(2, math.trunc(days / 3))
+
     return {
         data: {},
         "impact": {
