45983b721fbae54052f0accbbe8e380c58ae0ddb
kamula <kamulaisaac@gmail.com>
2020-04-10 15:05:50 +0300

    trunc

--- src/estimator.py
+++ src/estimator.py
@@ -3,18 +3,17 @@ import math
 
 def estimator(data):
     # function to calculate the number of currently infected people
+    trunk = math.trunc(data['reportedCases'])
 
     return {
         data: data,
         "impact": {
             "currentlyInfected": data['reportedCases'] * 10,
-            'infectionsByRequestedTime': (data['reportedCases'] * 10) * math.pow(2,
-                                                                                 math.trunc(data['reportedCases']))
+            'infectionsByRequestedTime': (data['reportedCases'] * 10) * math.pow(2, trunk)
 
         },
         "severeImpact": {
             "currentlyInfected": data['reportedCases'] * 50,
-            'infectionsByRequestedTime': (data['reportedCases'] * 50) * math.pow(2,
-                                                                                 math.trunc(data['reportedCases']))
+            'infectionsByRequestedTime': (data['reportedCases'] * 50) * math.pow(2, trunk)
         }
     }
