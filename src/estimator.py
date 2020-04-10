3e52ca6975e49c35a791a44c66f9fd01074ec7ef
kamula <kamulaisaac@gmail.com>
2020-04-10 15:32:04 +0300

    error

--- src/estimator.py
+++ src/estimator.py
@@ -1,8 +1,8 @@
 def estimator(data):
     impact = {}
     severeImpact = {}
-    impact.currentlyInfected = data.reportedCases * 10
-    severeImpact.currentlyInfected = data.reportedCases * 50
-    impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 9
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** 9
+    impact.currentlyInfected = data['reportedCases'] * 10
+    severeImpact.currentlyInfected = data['reportedCases'] * 50
+    impact.infectionsByRequestedTime = impact['currentlyInfected'] * 2 ** 9
+    severeImpact.infectionsByRequestedTime = severeImpact['currentlyInfected'] * 2 ** 9
 
