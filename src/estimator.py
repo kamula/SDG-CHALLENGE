c0b0cd5c60dd07a47ad7146997e387eb3b247a2a
kamula <kamulaisaac@gmail.com>
2020-04-10 16:03:37 +0300

    modified

--- src/estimator.py
+++ src/estimator.py
@@ -1,14 +1,10 @@
 def estimator(data):
     impact_current_infections = data['reportedCases'] * 10
     severe_impact_infected = data['reportedCases'] * 50
-    return {
-        'impact': {
-            'currentlyInfected': impact_current_infections,
-            'infectionsByRequestedTime': impact_current_infections * 512
-        },
-        'severeImpact': {
-            'currentlyInfected': severe_impact_infected,
-            'infectionsByRequestedTime': severe_impact_infected * 512
+    impact = {}
+    severeImpact = {}
+    impact['currentlyInfected'] = impact_current_infections
+    impact['currentlyInfected'] = impact_current_infections *512
+    severeImpact['currentlyInfected'] = severe_impact_infected
+    severeImpact['infectionsByRequestedTime'] = severe_impact_infected * 512
 
-        }
-    }
