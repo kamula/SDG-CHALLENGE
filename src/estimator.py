abbf8172bd7b7d1ee031ce95d417f72770198c46
kamula <kamulaisaac@gmail.com>
2020-04-10 14:56:04 +0300

    removed function

--- src/estimator.py
+++ src/estimator.py
@@ -3,18 +3,18 @@ import math
 
 def estimator(data):
     # function to calculate the number of currently infected people
-    def current_infections():
-        return {
-            data: {},
-            "impact": {
-                "currentlyInfected": data['reportedCases'] * 10,
-                'infectionsByRequestedTime': (data['reportedCases'] * 10) * math.pow(2,
-                                                                                     math.trunc(data['reportedCases']))
 
-            },
-            "severeImpact": {
-                "currentlyInfected": data['reportedCases'] * 50,
-                'infectionsByRequestedTime': (data['reportedCases'] * 50) * math.pow(2,
-                                                                                     math.trunc(data['reportedCases']))
-            }
+    return {
+        data: {},
+        "impact": {
+            "currentlyInfected": data['reportedCases'] * 10,
+            'infectionsByRequestedTime': (data['reportedCases'] * 10) * math.pow(2,
+                                                                                 math.trunc(data['reportedCases']))
+
+        },
+        "severeImpact": {
+            "currentlyInfected": data['reportedCases'] * 50,
+            'infectionsByRequestedTime': (data['reportedCases'] * 50) * math.pow(2,
+                                                                                 math.trunc(data['reportedCases']))
         }
+    }
