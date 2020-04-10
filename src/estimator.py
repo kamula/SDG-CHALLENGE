91e986c28b5f42fcc4faef200d2c88c515cea268
kamula <kamulaisaac@gmail.com>
2020-04-10 15:27:45 +0300

    trial 2

--- src/estimator.py
+++ src/estimator.py
@@ -1,19 +1,8 @@
-import math
-
-
 def estimator(data):
-    # function to calculate the number of currently infected people
-    trunk = math.trunc(data['reportedCases'])
+    impact = {}
+    severeImpact = {}
+    impact.currentlyInfected = data.reportedCases * 10
+    severeImpact.currentlyInfected = data.reportedCases * 50
+    impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 9
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** 9
 
-    return {
-        data: input(data),
-        "impact": {
-            "currentlyInfected": data['reportedCases'] * 10,
-            'infectionsByRequestedTime': (data['reportedCases'] * 10) * math.pow(2, trunk)
-
-        },
-        "severeImpact": {
-            "currentlyInfected": data['reportedCases'] * 50,
-            'infectionsByRequestedTime': (data['reportedCases'] * 50) * math.pow(2, trunk)
-        }
-    }
