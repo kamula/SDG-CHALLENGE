74a606c3353ba538ed9784ed8b839276e5a1684d
kamula <kamulaisaac@gmail.com>
2020-04-10 16:45:41 +0300

    corrected funtions

--- src/estimator.py
+++ src/estimator.py
@@ -1,10 +1,29 @@
-def estimator(data):
-    impact_current_infections = data['reportedCases'] * 10
-    severe_impact_infected = data['reportedCases'] * 50
-    impact = {}
-    severeImpact = {}
-    impact['currentlyInfected'] = impact_current_infections
-    impact['currentlyInfected'] = impact_current_infections *512
-    severeImpact['currentlyInfected'] = severe_impact_infected
-    severeImpact['infectionsByRequestedTime'] = severe_impact_infected * 512
+import math
 
+
+def estimator(**data):
+    def reported_cases_ten():
+        return data['reportedCases'] * 10
+
+    def reported_cases_fifty():
+        return data['reportedCases'] * 50
+
+    def impact_severe():
+        value = reported_cases_ten()
+        return value * math.pow(2, 9)
+
+    def severe_impact():
+        value2 = reported_cases_fifty()
+        return value2 * math.pow(2, 9)
+
+    return {
+        'impact': {
+            'currentlyInfected': reported_cases_ten(),
+            'infectionsByRequestedTime': impact_severe()
+        },
+        'severeImpact': {
+            'currentlyInfected': reported_cases_fifty(),
+            'infectionsByRequestedTime': severe_impact()
+        }
+
+    }
