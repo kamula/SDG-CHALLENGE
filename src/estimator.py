bafc918872a8c09381c361804a60116950cfd3b1
kamula <kamulaisaac@gmail.com>
2020-04-10 14:49:47 +0300

    deleted functions

--- src/estimator.py
+++ src/estimator.py
@@ -4,31 +4,17 @@ import math
 def estimator(data):
     # function to calculate the number of currently infected people
     def current_infections():
-        return int(data['reportedCases'] * 10)
+        return {
+            data: {},
+            "impact": {
+                "currentlyInfected": data['reportedCases'] * 10,
+                'infectionsByRequestedTime': (data['reportedCases'] * 10) * math.pow(2,
+                                                                                     math.trunc(data['reportedCases']))
 
-    # function to calculate 50 * more people infected
-    def more_people():
-        return int(data['reportedCases'] * 50)
-
-    # infectionsByRequestedTime for impact['currently infected]
-    def infections_by_time(days):
-        infections = current_infections()
-        return infections * math.pow(2, math.trunc(int(days) / 3))
-
-    # infections for servere impact
-    def infections_by_severe_impact(days):
-        infections = more_people()
-        return infections * math.pow(2, math.trunc(int(days) / 3))
-
-    return {
-        data: {},
-        "impact": {
-            "currentlyInfected": current_infections(),
-            'infectionsByRequestedTime': infections_by_time(data['reportedCases'])
-
-        },
-        "severeImpact": {
-            "currentlyInfected": more_people(),
-            'infectionsByRequestedTime': infections_by_severe_impact(data['reportedCases'])
+            },
+            "severeImpact": {
+                "currentlyInfected": data['reportedCases'] * 50,
+                'infectionsByRequestedTime': (data['reportedCases'] * 50) * math.pow(2,
+                                                                                     math.trunc(data['reportedCases']))
+            }
         }
-    }
