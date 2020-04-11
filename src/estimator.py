462120ce7d39e0349b2d88697dfa619149003f49
kamula <kamulaisaac@gmail.com>
2020-04-11 20:24:03 +0300

    reference error fixed

--- src/estimator.py
+++ src/estimator.py
@@ -8,32 +8,40 @@ def estimator(data):
     }
 
     # calculate the number of currently infected people
-    estimate.impact['currentlyInfected'] = data['reportedCases'] * 10
+    estimate['impact']['currentlyInfected'] = data['reportedCases'] * 10
 
     # severe impact
-    estimate.severeImpact['currentlyInfected'] = data['reportedCases'] * 50
+    estimate['severeImpact']['currentlyInfected'] = data['reportedCases'] * 50
 
     # calculate the number of infections in days, weeks and months
     period = data['timeToElapse']
     if data['periodType'] == 'days':
-        estimate.impact['infectionsByRequestedTime'] = estimate.impact['currentlyInfected'] * (2 ** math.trunc(period / 3))
+        estimate['impact']['infectionsByRequestedTime'] = estimate['impact']['currentlyInfected'] * (
+                    2 ** math.trunc(period / 3))
 
-        estimate.severeImpact['infectionsByRequestedTime'] = estimate.severeImpact['currentlyInfected'] * (2 ** math.trunc(period / 3))
+        estimate['severeImpact']['infectionsByRequestedTime'] = estimate['severeImpact']['currentlyInfected'] * (
+                    2 ** math.trunc(period / 3))
 
     elif data['periodType'] == 'weeks':
         period_in_days = period * 7
-        estimate.impact['infectionsByRequestedTime'] = estimate.impact['currentlyInfected'] * math.pow(2, math.trunc(period_in_days / 3))
+        estimate['impact']['infectionsByRequestedTime'] = estimate['impact']['currentlyInfected'] * math.pow(2,
+                                                                                                             math.trunc(
+                                                                                                                 period_in_days / 3))
 
         # severe impacts in weeks
-        estimate.severeImpact['infectionsByRequestedTime'] = estimate.severeImpact['currentlyInfected'] * (
+        estimate['severeImpact']['infectionsByRequestedTime'] = estimate['severeImpact']['currentlyInfected'] * (
                 2 ** math.trunc(period_in_days / 3))
 
     elif data['periodType'] == 'months':
         month_days = period * 30
-        estimate.impact['infectionsByRequestedTime'] = estimate.impact['currentlyInfected'] * math.pow(2, math.trunc(month_days / 3))
+        estimate['impact']['infectionsByRequestedTime'] = estimate['impact']['currentlyInfected'] * math.pow(2,
+                                                                                                             math.trunc(
+                                                                                                                 month_days / 3))
 
         # severe impact in months
-        estimate.severeImpact['infectionsByRequestedTime'] = estimate.severeImpact['currentlyInfected'] * (
+        estimate['severeImpact']['infectionsByRequestedTime'] = estimate['severeImpact']['currentlyInfected'] * (
                 2 ** math.trunc(month_days / 3))
 
     return estimate
+
+
